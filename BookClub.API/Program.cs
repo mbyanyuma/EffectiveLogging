b768b09ab3f3945df3c1cc96f7de4007598963ae
Moses Byanyuma <mbyanyuma108@gmail.com>
2019-11-27 21:46:53 -0500

    installed serilog, wrote initial serilog entries in API and UI program files

--- BookClub.API/Program.cs
+++ BookClub.API/Program.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Formatting.Json;
 
 namespace BookClub.API
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .WriteTo.File(new JsonFormatter(), "c:\\temp\\logs\\book-club.json", shared: true)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting the web host");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host was terminated unexpectedly, Moses ): ");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- BookClub.UI/Program.cs
+++ BookClub.UI/Program.cs
@@ -1,8 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Json;
 
 namespace BookClub.UI
 {
@@ -16,7 +19,26 @@ namespace BookClub.UI
 
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .WriteTo.File(new JsonFormatter(), "c:\\temp\\logs\\book-club.json", shared: true)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting the web host");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host was terminated unexpectedly, Moses ): ");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
